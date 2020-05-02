d7309fda0a9f0acc16191e0ffb361a45570146d6
Jose Vwlito Ruiz - TISMART <josevelito@bcp.com.pe>
2020-05-02 18:06:28 -0500

    menu responsive and scroll effects

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+window.onload = function() {
+    let handlers = document.getElementsByClassName('c-sidebar-navigator__link');
+    this.console.log(handlers.length);
+    for( let i = 0; i < handlers.length; i++) {
+        console.log(handlers[i].text);
+        handlers[i].onclick = function(event) {
+            event.preventDefault();
+            let elem = event.target;
+            let destiny = elem.getAttribute('href');
+            let content = document.getElementById(destiny);
+            let offTop = content.offsetTop;
+            console.log(offTop);
+            window.scrollTo({top: offTop, behavior: 'smooth'});
+            closeNav();
+        }
+    } 
+}
+
 function showMenu() {
   console.log('working menu');
   var el = document.getElementById("main-menu");
@@ -6,10 +24,14 @@ function showMenu() {
 
 function openNav() {
   document.getElementById("mySidenav").style.width = "320px";
-  document.getElementById("content-nav-lateral").style.marginLeft = "320px";
+//   document.getElementById("content-nav-lateral").style.marginLeft = "320px";
 }
   
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("content-nav-lateral").style.marginLeft= "0";
+//   document.getElementById("content-nav-lateral").style.marginLeft= "0";
+}
+
+function scrolling() {
+    console.log('hi i\'m scrollign');
 }
