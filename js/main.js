523eec4f83feabf215e6f53236848be6221c9306
AlexanderLlanos <alexander.asdfg@gmail.com>
2020-05-02 16:07:57 -0500

    nav lateral

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 function showMenu() {
-    console.log('working menu');
-    var el = document.getElementById("main-menu");
-    el.classList.toggle("active");
-  }
+  console.log('working menu');
+  var el = document.getElementById("main-menu");
+  el.classList.toggle("active");
+}
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "320px";
+  document.getElementById("content-nav-lateral").style.marginLeft = "320px";
+}
+  
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("content-nav-lateral").style.marginLeft= "0";
+}
